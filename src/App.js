d59299cc8f83d0ca37c7ad5a8e188efec97a164c
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-02-22 18:17:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import FAQ from './components/Faq/FAQ';
 
 function App() {
   return (
-   <div className="App" style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white' }}>
+   <div className="App" style={{ backgroundColor: 'black', minHeight: '100vh' }}>
       <Navbar />
       <HeroSection />
       <NewAlbumsSection />
