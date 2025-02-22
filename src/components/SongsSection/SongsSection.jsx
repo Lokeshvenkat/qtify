2a1a69b76ac325e1582b1399a4a6c20d7ed8ac2b
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-02-22 13:13:08 +0530

    Update SongsSection.jsx

--- src/components/SongsSection/SongsSection.jsx
+++ src/components/SongsSection/SongsSection.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Tabs, Tab } from '@mui/material';
 import Carousel from '../Carousel/Carousel';
 import styles from './SongsSection.module.css';
 
