a6be02514c52de1f6b25566ffd28e497b2ee24d6
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-02-22 18:16:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,8 +12,6 @@ function App() {
   return (
    <div className="App" style={{ backgroundColor: 'black', minHeight: '100vh', color: 'white' }}>
       <Navbar />
-      <Logo />
-      <SearchBar />
       <HeroSection />
       <NewAlbumsSection />
       <SongsSection />
