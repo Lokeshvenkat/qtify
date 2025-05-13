98d45bd7fce8684226d2a63b6e528597c62897df
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-05-13 18:38:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import HomePage from "./Pages/Homepage/Homepage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Feedback from "./Components/Feedback/Feedback";
 
-const container = document.getElementById('root');
-if (container) {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <Router>
-        <App />
-      </Router>
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element not found");
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/Feedback",
+        element: <Feedback />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
