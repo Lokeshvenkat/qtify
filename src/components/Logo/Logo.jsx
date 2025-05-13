f3cf2c9e14ee0aa87340780422657f2a48ffd373
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-05-13 18:44:22 +0530

    Delete src/components/Logo directory

--- src/components/Logo/Logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import logoImage from '../../assets/logo.png';
-import styles from './Logo.module.css';
-
-const Logo = () => {
-  return <img src={logoImage} alt="QTify Logo" className={styles.logo} />;
-};
-
-export default Logo;
