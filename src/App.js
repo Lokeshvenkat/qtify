09b180e1cae20140713c0764c31be19f141dffcc
Lokesh Venkat <46650625+Lokeshvenkat@users.noreply.github.com>
2025-05-13 18:38:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import Navbar from './components/Navbar/Navbar';
-import HeroSection from './components/HeroSection/HeroSection';
-import Section from './components/Section/Section';
-import SongsSection from './components/SongsSection/SongsSection';
-import NewAlbumsSection from './components/NewAlbumsSection/NewAlbumsSection';
-import Logo from './components/Logo/Logo.jsx';
-import SearchBar from './components/SearchBar/SearchBar';
-import FAQ from './components/Faq/FAQ';
+import React, { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import { fetchNewAlbums, fetchSongs, fetchTopAlbums } from "./Apidata/api";
+import { StyledEngineProvider } from "@mui/material";
 
 function App() {
-  
+  const [data, setData] = useState({});
 
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevData) => {
+        return { ...prevData, [key]: data };
+      });
+    });
+  };
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [] } = data;
   return (
-    <div className="App" style={{ backgroundColor: 'black', minHeight: '100vh' }}>
-      <Navbar />
-      <HeroSection />
-      <NewAlbumsSection />
-      <SongsSection />
-      <FAQ />
-    </div>
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...topAlbums, ...newAlbums]} />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs } }} />
+      </StyledEngineProvider>
+    </>
   );
 }
 
